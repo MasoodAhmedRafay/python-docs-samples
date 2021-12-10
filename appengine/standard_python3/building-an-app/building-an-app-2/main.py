1c14d9242c8c61bd3f9a2e401e248d95240209bc
Masood Ahmed Rafay <32571814+MasoodAhmedRafay@users.noreply.github.com>
2021-12-10 15:42:23 +0100

    test-code

--- appengine/standard_python3/building-an-app/building-an-app-2/main.py
+++ appengine/standard_python3/building-an-app/building-an-app-2/main.py
@@ -12,66 +12,47 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import datetime
+# [START gae_python38_render_template]
+# [START gae_python3_render_template]
+#import datetime
 
 from flask import Flask, render_template
 
-# [START gae_python38_datastore_store_and_fetch_times]
-# [START gae_python3_datastore_store_and_fetch_times]
-from google.cloud import datastore
-
-datastore_client = datastore.Client()
-
-# [END gae_python3_datastore_store_and_fetch_times]
-# [END gae_python38_datastore_store_and_fetch_times]
 app = Flask(__name__)
 
 
-# [START gae_python38_datastore_store_and_fetch_times]
-# [START gae_python3_datastore_store_and_fetch_times]
-def store_time(dt):
-    entity = datastore.Entity(key=datastore_client.key('visit'))
-    entity.update({
-        'timestamp': dt
-    })
-
-    datastore_client.put(entity)
-
-
-def fetch_times(limit):
-    query = datastore_client.query(kind='visit')
-    query.order = ['-timestamp']
-
-    times = query.fetch(limit=limit)
-
-    return times
-# [END gae_python3_datastore_store_and_fetch_times]
-# [END gae_python38_datastore_store_and_fetch_times]
-
-
-# [START gae_python38_datastore_render_times]
-# [START gae_python3_datastore_render_times]
 @app.route('/')
-def root():
-    # Store the current access time in Datastore.
-    store_time(datetime.datetime.now(tz=datetime.timezone.utc))
+DEFAULT_STUDENTS = (
+    'Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred',
+    'Ginny', 'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry',
+)
+PLANTS = {
+    'C': 'Clover',
+    'G': 'Grass',
+    'R': 'Radishes',
+    'V': 'Violets',
+}
 
-    # Fetch the most recent 10 access times from Datastore.
-    times = fetch_times(10)
 
-    return render_template(
-        'index.html', times=times)
-# [END gae_python3_datastore_render_times]
-# [END gae_python38_datastore_render_times]
+class Garden(object):
+    def __init__(self, diagram, students=DEFAULT_STUDENTS):
+        self.lines = diagram.splitlines()
+        self.students = sorted(students)
+
+    def plants(self, student):
+        index = self.students.index(student) * 2
+        return [PLANTS[p[i]] for p in self.lines for i in (index, index + 1)]
 
 
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
+garden = Garden("VCRRGVRG\nRVGCCGCV")
+print(garden.students)
+print(garden.lines)
+print(garden.plants("Alice"))
 
-    # Flask's development server will automatically serve static files in
-    # the "static" directory. See:
-    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
-    # App Engine itself will serve those files as configured in app.yaml.
-    app.run(host='127.0.0.1', port=8080, debug=True)
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+
+
+# [END gae_python3_render_template]
+# [END gae_python38_render_template]
